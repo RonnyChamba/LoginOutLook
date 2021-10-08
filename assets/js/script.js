40952c500d85b11a9089bfc38316f4eab382ea0b
RC <ronnychamba96@gmail.com>
2021-10-07 19:00:31 -0500

    disegnio-dos

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,45 @@ document.addEventListener("DOMContentLoaded", () => {
   [...$contentCards.children].forEach((card) => {
     card.addEventListener("click", (event) => {
       if (event.target.matches("button")) {
-        let anchoCard = getComputedStyle(card).getPropertyValue("width");
-        let mLeft =
-          getComputedStyle($contentCards).getPropertyValue("margin-left");
-
-        if (parseInt(mLeft) !== 0) {
-          anchoCard = Math.abs(parseInt(mLeft) - parseInt(anchoCard)) + "px";
-        }
-        $contentCards.style.marginLeft = `-${anchoCard}`;
-        console.log("Magen left actual: ", $contentCards.style.marginLeft);
+        clickButton(card);
       }
-
       if (event.target.matches(".main-content__return span")) {
-        let anchoCard = getComputedStyle(card).getPropertyValue("width");
-        let mLeft =
-          getComputedStyle($contentCards).getPropertyValue("margin-left");
-
-        anchoCard =
-          "-" + Math.abs(parseInt(mLeft) + parseInt(anchoCard)) + "px";
-        $contentCards.style.marginLeft = `${anchoCard}`;
+        clickReturn(card);
       }
     });
   });
+
+  const setMail = (card) => {
+    let email = card.querySelector(".input-mail").value;
+    let tipoEmail = card.querySelector(".tipo-mail");
+    let index = tipoEmail.selectedIndex;
+    let opcionSeleccionada = tipoEmail.options[index].text;
+    // let valueSeleccionada = tipoEmail.options[index].value;
+    document.querySelectorAll(".main-content__return__mail").forEach((x) => {
+      x.textContent = `${email}${opcionSeleccionada}`;
+    });
+  };
+
+  const clickButton = (card) => {
+    if ($contentCards.firstElementChild === card) setMail(card);
+
+    if ($contentCards.lastElementChild === card) {
+      //  setMail(card);
+      return;
+    }
+    let anchoCard = getComputedStyle(card).getPropertyValue("width");
+    let mLeft = getComputedStyle($contentCards).getPropertyValue("margin-left");
+
+    if (parseInt(mLeft) !== 0) {
+      anchoCard = Math.abs(parseInt(mLeft) - parseInt(anchoCard)) + "px";
+    }
+    $contentCards.style.marginLeft = `-${anchoCard}`;
+  };
+
+  const clickReturn = (card) => {
+    let anchoCard = getComputedStyle(card).getPropertyValue("width");
+    let mLeft = getComputedStyle($contentCards).getPropertyValue("margin-left");
+    anchoCard = "-" + Math.abs(parseInt(mLeft) + parseInt(anchoCard)) + "px";
+    $contentCards.style.marginLeft = `${anchoCard}`;
+  };
 });
